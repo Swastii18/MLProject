d67bd875b09efb5cf9a0b98958e5ebcae56ff683
saroj shrestha <swastiishrestha30@gmail.com>
2024-03-16 23:02:05 +0545

    data transformation works

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,10 +3,12 @@ import sys
 from src.exception import CustomException
 from src.logger  import logging
 import pandas as pd
-import numpy as np
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -45,4 +47,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_tranformation=DataTransformation()
+    data_tranformation.initiate_data_transformation(train_data,test_data)
